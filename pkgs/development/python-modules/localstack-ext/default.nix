4ec63d352c43303f3ae0b17fa7895f42d6df407d
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:06:20 +0200

    python3Packages.localstack-ext: 2.3.2 -> 3.5.0

--- pkgs/development/python-modules/localstack-ext/default.nix
+++ pkgs/development/python-modules/localstack-ext/default.nix
@@ -17,12 +17,12 @@
 
 buildPythonPackage rec {
   pname = "localstack-ext";
-  version = "2.3.2";
+  version = "3.5.0";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-Ex5ZPlteDaiyex90QumucVdTTbpp9uWiBrvw1kMr++8=";
+    hash = "sha256-tJNgNqCzlebbDOOgkScOKaf04bLTVIjP+BaFiDLo/Gs=";
   };
 
   postPatch = ''
