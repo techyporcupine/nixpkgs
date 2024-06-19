03ca0fc4e8a8f9f58a60b7011d20fecb0c368878
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:05:24 +0200

    python3Packages.dj-database-url: 2.1.0 -> 2.2.0
    https://github.com/jazzband/dj-database-url/blob/v2.2.0/CHANGELOG.md

--- pkgs/development/python-modules/dj-database-url/default.nix
+++ pkgs/development/python-modules/dj-database-url/default.nix
@@ -8,14 +8,14 @@
 
 buildPythonPackage rec {
   pname = "dj-database-url";
-  version = "2.1.0";
+  version = "2.2.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-8gQs7+EIblOcnaOfrVrX9hFzv3lmXmm/fk3lX6iLE18=";
+    hash = "sha256-n5sFBY3fiI8eb4QASLjXBf+TleO1KgcWXao9i5NgVRs=";
   };
 
   propagatedBuildInputs = [ django ];
