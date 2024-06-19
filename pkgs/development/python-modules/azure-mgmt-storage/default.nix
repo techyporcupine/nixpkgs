7b8750211fcf328ed10bab5b3438f029ea2d13bc
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:04:50 +0200

    python3Packages.azure-mgmt-storage: 21.1.0 -> 21.2.0

--- pkgs/development/python-modules/azure-mgmt-storage/default.nix
+++ pkgs/development/python-modules/azure-mgmt-storage/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "azure-mgmt-storage";
-  version = "21.1.0";
+  version = "21.2.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-1tPA6RfJiLye0Eckd9PvP5CIYAnrHZenEZRPg3VjAWI=";
+    hash = "sha256-KHyYQLAb6TGBnUA9p+1SvWL9B3sFKd1HDm28T+3ksg0=";
   };
 
   propagatedBuildInputs = [
