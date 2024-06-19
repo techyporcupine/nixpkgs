e5339d5468b79a4c744a26800e1944d88f11ee06
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:07:17 +0200

    python3Packages.setuptools-scm: 8.0.4 -> 8.1.0
    https://github.com/pypa/setuptools_scm/blob/8.1.0/CHANGELOG.md

--- pkgs/development/python-modules/setuptools-scm/default.nix
+++ pkgs/development/python-modules/setuptools-scm/default.nix
@@ -19,12 +19,12 @@
 
 buildPythonPackage rec {
   pname = "setuptools-scm";
-  version = "8.0.4";
+  version = "8.1.0";
   pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-tfQ/9oAGaVlRk/0JiRVk7p0dfcsZbKtLJQbVOi4clcc=";
+    hash = "sha256-Qt6htldxy6k7elFdZaZdgkblYHaKZrkQalksjn8myKc=";
   };
 
   nativeBuildInputs = [ setuptools ] ++ lib.optionals (pythonOlder "3.11") [ tomli ];
