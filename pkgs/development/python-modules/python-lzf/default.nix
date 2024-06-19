1807b528c67e92fcc29742ab5994bb472058c786
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:07:08 +0200

    python3Packages.python-lzf: 0.2.4 -> 0.2.6

--- pkgs/development/python-modules/python-lzf/default.nix
+++ pkgs/development/python-modules/python-lzf/default.nix
@@ -5,13 +5,13 @@
 }:
 
 buildPythonPackage rec {
-  version = "0.2.4";
+  version = "0.2.6";
   format = "setuptools";
   pname = "python-lzf";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "1l8m6vzwm1m8hn7ldw8j8r2b6r199k8z3q0wnhdyy4p68hahyhni";
+    sha256 = "sha256-R9tcLLNxvcRfYcyj4SFUylp6f7uOGjr45cYtFBKdodA=";
   };
 
   meta = with lib; {
