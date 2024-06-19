7f3544cdce80df8668b8663be731dd712cefb1af
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-19 05:04:32 +0200

    python3Packages.aioazuredevops: 2.0.0 -> 2.2.0
    https://github.com/timmo001/aioazuredevops/releases/tag/2.2.0

--- pkgs/development/python-modules/aioazuredevops/default.nix
+++ pkgs/development/python-modules/aioazuredevops/default.nix
@@ -22,7 +22,7 @@
 
 buildPythonPackage rec {
   pname = "aioazuredevops";
-  version = "2.0.0";
+  version = "2.2.0";
   pyproject = true;
 
   disabled = pythonOlder "3.11";
@@ -31,7 +31,7 @@ buildPythonPackage rec {
     owner = "timmo001";
     repo = "aioazuredevops";
     rev = "refs/tags/${version}";
-    hash = "sha256-QEIVAcBoTvuOeLN2kfDa3uYfrUm5Qu1TLp9C0uU+mW4=";
+    hash = "sha256-1v58I9WOyyrp9n+qdvVeMZ3EObqP/06XCOZYS0nEvPU=";
   };
 
   build-system = [
