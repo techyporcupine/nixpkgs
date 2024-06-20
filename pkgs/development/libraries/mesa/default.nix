16088ec190b26a89aef5425e657a47e4ba24f1ef
natsukium <tomoya.otabi@gmail.com>
2024-06-20 16:18:59 +0900

    mesa: fix build with python312

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -278,6 +278,7 @@ in stdenv.mkDerivation {
     flex
     file
     python3Packages.python
+    python3Packages.packaging
     python3Packages.pycparser
     python3Packages.mako
     python3Packages.ply
