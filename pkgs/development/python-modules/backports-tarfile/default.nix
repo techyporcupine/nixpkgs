2f949458ac6519382bfa03212b25ac92491eb749
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-06-20 02:05:41 +0200

    fixup! python312Packages.backports-tarfile: init at 1.2.0

--- pkgs/development/python-modules/backports-tarfile/default.nix
+++ pkgs/development/python-modules/backports-tarfile/default.nix
@@ -34,7 +34,24 @@ let self = buildPythonPackage rec {
     pytestCheckHook
   ];
 
-  pythonImportsCheck = [ "tarfile" ];
+  disabledTests = [
+    # calls python -m backports.tarfile and doesn't find module documentation
+    "test_bad_use"
+    "test_create_command"
+    "test_create_command_compressed"
+    "test_create_command_dot_started_filename"
+    "test_create_command_dotless_filename"
+    "test_extract_command"
+    "test_extract_command_different_directory"
+    "test_extract_command_invalid_file"
+    "test_list_command_invalid_file"
+    "test_test_command"
+    "test_test_command_invalid_file"
+    # chmod: permission denied
+    "test_modes"
+  ];
+
+  pythonImportsCheck = [ "backports.tarfile" ];
 
   passthru.tests.pytest = self.overridePythonAttrs { doCheck = true; };
 
